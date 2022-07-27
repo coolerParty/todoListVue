37c7171d3a99929665113338bf700a5e299d0dad
Alex Meier <92195211+coolerParty@users.noreply.github.com>
2022-07-27 22:32:44 +0800

    component trial not done.

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,16 @@
 require('./bootstrap');
+
+import {createApp} from 'vue/dist/vue.esm-bundler';
+
+import ExampleComponent1 from './components/ExampleComponent1.vue';
+
+const rootComponent1 = createApp({
+        data() {
+            return {
+                message: 'Hello root Component 1'
+            };
+        },
+        components: {ExampleComponent1},
+    });
+rootComponent1.mount('#root-component-1');
+
---
+++ resources/js/vue/ExampleComponent1.vue
@@ -0,0 +1,10 @@
+<template>
+<div>
+    I'm an example component 1.
+</div>
+</template>
+<script>
+    export default {
+
+    }
+</script>
